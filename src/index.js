5dedfd30a45d147720163e511e51cbb4dbffd685
Erik Huang <erikhuangdrums@gmail.com>
2021-02-23 15:05:54 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const form = document.querySelector('#create-task-form')
+const list = document.querySelector('ul#tasks')
+
+const createLI = val => {
+  let li = document.createElement('li')
+  list.appendChild(li)
+  li.innerText = val
+  li.style.fontSize = '200px'
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let input = document.querySelector('input#new-task-description').value
+    createLI(input)
+  })
 });
